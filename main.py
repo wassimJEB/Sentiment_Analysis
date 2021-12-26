fb4e0cd01d7151704fec6351dcd36c7da04264ae
Wassim Jebali <63062374+skelton98@users.noreply.github.com>
2021-12-26 17:33:44 +0100

    add tweet scrapping

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 import pandas as pd
 import altair as alt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+from TweetScrapping import *
 
 # Fxn
 def convert_to_df(sentiment):
@@ -35,33 +35,70 @@ def analyze_token_sentiment(docx):
 
 
 def main():
-    st.title("Sentiment Analysis NLP App using Vader API + STREAMLIT realized byWassim JEBALI")
-    with st.form(key='nlpForm'):
-        raw_text = st.text_area("Enter Text Here")
-        submit_button = st.form_submit_button(label='Analyze')
+    st.title("Sentiment Analysis NLP App using  STREAMLIT realized by Wassim JEBALI")
+    c = st.selectbox("Choisir une option", ["Tweet scrapping and analysis ", "Sentiment Analysis NLP App using Vader API"])
+    if c== "Sentiment Analysis NLP App using Vader API":
+        with st.form(key='nlpForm'):
+            raw_text = st.text_area("Enter Text Here")
+            submit_button = st.form_submit_button(label='Analyze')
 
-        # layout
-        col1, col2 = st.columns(2)
-        if submit_button:
+            # layout
+            col1, col2 = st.columns(2)
+            if submit_button:
 
-            with col1:
-                st.info("Results")
-                sentiment = TextBlob(raw_text).sentiment
-                # Emoji
-                if sentiment.polarity > 0:
-                    st.markdown("Sentiment:: Positive :smiley: ")
-                elif sentiment.polarity < 0:
-                    st.markdown("Sentiment:: Negative :angry: ")
-                else:
-                    st.markdown("Sentiment:: Neutral 😐 ")
+                with col1:
+                    st.info("Results")
+                    sentiment = TextBlob(raw_text).sentiment
+                    # Emoji
+                    if sentiment.polarity > 0:
+                        st.markdown("Sentiment:: Positive :smiley: ")
+                    elif sentiment.polarity < 0:
+                        st.markdown("Sentiment:: Negative :angry: ")
+                    else:
+                        st.markdown("Sentiment:: Neutral 😐 ")
 
 
 
-            with col2:
-                st.info("Token Sentiment")
+                with col2:
+                    st.info("Token Sentiment")
 
-                token_sentiments = analyze_token_sentiment(raw_text)
-                st.write(token_sentiments)
+                    token_sentiments = analyze_token_sentiment(raw_text)
+                    st.write(token_sentiments)
+
+    else:
+        with st.form(key='nlpForm'):
+
+
+            raw_text = st.text_area("Enter Keyword/Tag to search about")
+            nb = st.text_area("Enter number of tweets")
+            button = st.form_submit_button(label='Analyze')
+
+
+
+            # layout
+            col1, col2 = st.columns(2)
+            if button:
+                objet = SentimentAnalysis()
+                objet.DownloadData(raw_text, nb)
+
+                with col1:
+                    st.info("Results")
+                    sentiment = TextBlob(raw_text).sentiment
+                    # Emoji
+                    if sentiment.polarity > 0:
+                        st.markdown("Sentiment:: Positive :smiley: ")
+                    elif sentiment.polarity < 0:
+                        st.markdown("Sentiment:: Negative :angry: ")
+                    else:
+                        st.markdown("Sentiment:: Neutral 😐 ")
+
+                with col2:
+                    st.info("Token Sentiment")
+
+                    token_sentiments = analyze_token_sentiment(raw_text)
+                    st.write(token_sentiments)
+
+
 
 
 
